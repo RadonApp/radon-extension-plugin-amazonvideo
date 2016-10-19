ff5f54e941c6bfc816ec8e0d6c76cb52fdac6f45
Dean Gardiner <me@dgardiner.net>
2016-10-20 10:21:48 +1300

    Updated configuration service to use preference pages

--- src/services/configuration/options.js
+++ src/services/configuration/options.js
@@ -1,31 +1,33 @@
 import {
-    CheckboxOption,
+    Group,
+    Page,
     EnableOption,
-    SelectOption,
-    Group
+    SelectOption
 } from 'eon.extension.framework/services/configuration/models';
 
 import Plugin from '../../core/plugin';
 
 
 export default [
-    new EnableOption(Plugin, 'enabled', 'Enabled', {
-        default: false,
+    new Page(Plugin, null, Plugin.title, [
+        new EnableOption(Plugin, 'enabled', 'Enabled', {
+            default: false,
 
-        contentScripts: Plugin.contentScripts,
-        permissions: Plugin.permissions
-    }),
+            contentScripts: Plugin.contentScripts,
+            permissions: Plugin.permissions
+        }),
 
-    new Group(Plugin, 'developer', 'Developer', [
-        new SelectOption(Plugin, 'developer.log_level', 'Log Level', [
-            {key: 'error', label: 'Error'},
-            {key: 'warning', label: 'Warning'},
-            {key: 'notice', label: 'Notice'},
-            {key: 'info', label: 'Info'},
-            {key: 'debug', label: 'Debug'},
-            {key: 'trace', label: 'Trace'}
-        ], {
-            default: 'warning'
-        })
+        new Group(Plugin, 'developer', 'Developer', [
+            new SelectOption(Plugin, 'developer.log_level', 'Log Level', [
+                {key: 'error', label: 'Error'},
+                {key: 'warning', label: 'Warning'},
+                {key: 'notice', label: 'Notice'},
+                {key: 'info', label: 'Info'},
+                {key: 'debug', label: 'Debug'},
+                {key: 'trace', label: 'Trace'}
+            ], {
+                default: 'warning'
+            })
+        ])
     ])
 ];
